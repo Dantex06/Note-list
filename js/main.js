77609fb3e2f5892c90f45c0983ccea878c967291
Egor <egorrogachev030@gmail.com>
2024-04-13 01:41:02 +0300

    full work notepad

--- js/main.js
+++ js/main.js
@@ -1,16 +1,21 @@
 const createButton = document.querySelector('.note__button');
 const lists = document.querySelector('.note__lists');
-createButton.addEventListener("click", ()=> {
+const modal = document.querySelector('.modal');
+
+function createNote(){
+    let title = document.querySelector('.create__title');
+    let description = document.querySelector('.create__description');
+    console.log(title, description)
     const list = document.createElement("div");
     list.classList.add('note__list')
     list.innerHTML = `
                 <div class="note__list-close">
                 </div>
                 <div class="note__list-title">
-                    Домашнее задание по математике
+                    ${title.value}
                 </div>
                 <div class="note__list-description">
-                    Теория параграф 2.3. Домашняя работа 1.2
+                    ${description.value}
                 </div>
     `
     const closeButton = list.querySelector('.note__list-close');
@@ -18,5 +23,25 @@ createButton.addEventListener("click", ()=> {
         list.remove();
     })
     lists.appendChild(list);
+    modal.classList.remove("modal--show");
+    document.querySelector('.create__title').value = '';
+    document.querySelector('.create__description').value = '';
+}
+
+
+createButton.addEventListener("click", ()=> {
+    modal.classList.add("modal--show");
+    const closeModal = modal.querySelector('.close__modal-button');
+    const createModal = modal.querySelector('.create__modal-button');
+
+    createModal.removeEventListener("click", createNote)
+    createModal.addEventListener("click", createNote)
+
+    closeModal.addEventListener("click", ()=>{
+        modal.classList.remove("modal--show")
+        console.log('hi')
+    });
+
+
 })
 
