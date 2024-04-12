12d44e0ed409672118b2e9c709423df896179edb
Egor <egorrogachev030@gmail.com>
2024-04-12 23:12:12 +0300

    function add note

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const createButton = document.querySelector('.note__button');
+const lists = document.querySelector('.note__lists');
+createButton.addEventListener("click", ()=> {
+    const list = document.createElement("div");
+    list.classList.add('note__list')
+    list.innerHTML = `
+                <div class="note__list-close">
+                </div>
+                <div class="note__list-title">
+                    Домашнее задание по математике
+                </div>
+                <div class="note__list-description">
+                    Теория параграф 2.3. Домашняя работа 1.2
+                </div>
+    `
+    lists.appendChild(list)
+})
